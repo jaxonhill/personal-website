8576972dc04e83f86278de91341c7fd098dd4f89
jaxonhill <jaxoncharleshill@gmail.com>
2022-11-26 02:38:08 -0800

    Add about button functionality

--- personal-website/tailwind.config.js
+++ personal-website/tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        "neubrutalism": "0 2px 0 rgba(24, 24, 27, 1.00)",
+      },
+      boxShadow: {
+        "neubrutalism": "0 2px 0 rgba(24, 24, 27, 1.00)",
+      }
+    },
     fontFamily: {
       "montserrat": "Montserrat, sans-serif",
     }
