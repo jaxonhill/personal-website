be969c97cc41293042b4edb4ac40866aaadb0061
jaxonhill <jaxoncharleshill@gmail.com>
2022-11-26 01:40:35 -0800

    Initial commit, add navbar and heading

---
+++ personal-website/src/HeadingSection.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const HeadingSection = () => {
+    return (
+        <div className="px-4 flex flex-col items-center">
+            <h1 className="text-4xl mb-6">Hey &#128075; I'm <span className="font-extrabold">Jaxon Hill</span></h1>
+            <div className="text-center">
+                <p>
+                    I'm a computer science student with a passion for designing
+                    and coding interactive applications. I always enjoy learning
+                    new technologies and originally started my coding journey as a
+                    self-taught programmer.
+                </p>
+                <br />
+                <p>
+                    My current focus is designing and building for web development,
+                    but in the coming years I'm looking forward to learning game
+                    development and machine learning, in and outside of school.
+                </p>
+            </div>
+            <div>
+            </div>
+        </div>
+    )
+}
+
+export default HeadingSection
