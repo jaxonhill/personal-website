363481a5c95e971ed22f715472e4a2aa93293858
jaxonhill <jaxoncharleshill@gmail.com>
2022-12-02 08:48:15 -0800

    Fix mobile styling overflow on heading buttons

--- frontend/src/components/AboutButton.js
+++ frontend/src/components/AboutButton.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { motion } from "framer-motion"
 
 const AboutButton = ({ aboutButton }) => {
+    const [windowWidth, setWindowWidth] = useState(0);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        }
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        }
+    }, [])
+
     return (
         <a href={aboutButton.href}>
             <motion.button
@@ -11,9 +24,9 @@ const AboutButton = ({ aboutButton }) => {
                     boxShadow: "2px 2px 0 0 rgba(24, 24, 27, 1.00)",
                 }}
                 whileTap={{ scale: 0.85 }}
-                className={`${aboutButton.bgColor} flex items-center py-2 px-5 gap-2 border-2 rounded-2xl border-zinc-900 shadow-neubrutalism-medium`}
+                className={`${aboutButton.bgColor} flex items-center p-4 md:py-2 md:px-5 gap-2 border-2 rounded-2xl border-zinc-900 shadow-neubrutalism-medium`}
             >
-                <span className="font-extrabold">{aboutButton.name}</span>
+                {windowWidth >= 640 && <span className="font-extrabold">{aboutButton.name}</span>}
                 <img src={aboutButton.svgPath} alt={aboutButton.name} />
             </motion.button>
         </a>
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,8 +3,8 @@ import { motion } from "framer-motion"
 
 const Navbar = () => {
     return (
-        <nav className="sticky bg-white top-0 flex items-center justify-between mb-16 border-b-2 border-b-zinc-900 pl-4 md:px-16 lg:px-32 2xl:px-48">
-            <p className="text-3xl py-2 md:pl-0 lg:py-4">
+        <nav className="sticky bg-white top-0 flex items-center justify-between mb-16 border-b-2 border-b-zinc-900 px-4 md:px-16 lg:px-32 2xl:px-48">
+            <p className="text-2xl py-2 md:text-3xl lg:py-4">
                 <span className="font-extrabold">JAXONHILL</span>
                 .xyz
             </p>
