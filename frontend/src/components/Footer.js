44c852d07d667e7e60b07e734a5b54dbc9059fd1
jaxonhill <jaxoncharleshill@gmail.com>
2022-12-02 02:57:01 -0800

    Add animations to footer buttons

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,18 +1,31 @@
 import React from 'react'
+import { motion } from "framer-motion"
 
 const Footer = () => {
     return (
         <div className="p-4 border-t-2 border-zinc-900">
             <div className="flex justify-center gap-4 items-center mb-2">
-                <a href="https://github.com/jaxonhill">
+                <motion.a
+                    whileHover={{ scale: 1.20 }}
+                    whileTap={{ scale: 0.85 }}
+                    href="https://github.com/jaxonhill"
+                >
                     <img src="./svgs/github_logo.svg" alt="Github" />
-                </a>
-                <a href="https://www.linkedin.com/in/jaxon-c-hill/">
+                </motion.a>
+                <motion.a
+                    whileHover={{ scale: 1.20 }}
+                    whileTap={{ scale: 0.85 }}
+                    href="https://www.linkedin.com/in/jaxon-c-hill/"
+                >
                     <img src="./svgs/linkedin_logo.svg" alt="LinkedIn" />
-                </a>
-                <a href="mailto:jaxoncharleshill@gmail.com">
+                </motion.a>
+                <motion.a
+                    whileHover={{ scale: 1.20 }}
+                    whileTap={{ scale: 0.85 }}
+                    href="mailto:jaxoncharleshill@gmail.com"
+                >
                     <img src="./svgs/email_logo.svg" alt="Email" />
-                </a>
+                </motion.a>
             </div>
             <p className="text-center">&copy; 2022 &bull; Jaxon Hill</p>
         </div>
